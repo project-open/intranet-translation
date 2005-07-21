c9d0ad93ff84736ceca59d2bd04b10715442fd1e
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-07-21 18:32:57 +0000

    - fixed the project list pagination - changed the project list to display start_date and end_date   formatted - Added support for Unicode Trados 7.0 format to trados import

--- www/trans-tasks/trados-import.tcl
+++ www/trans-tasks/trados-import.tcl
@@ -32,10 +32,8 @@ ad_page_contract {
 }
 
 # ---------------------------------------------------------------------
-# Defaults & security
+# Security
 # ---------------------------------------------------------------------
-
-ns_log Notice "trados-import: trados_wordcount_file=$trados_wordcount_file"
 
 set user_id [ad_maybe_redirect_for_registration]
 im_project_permissions $user_id $project_id view read write admin
@@ -43,6 +41,38 @@ if {!$write} {
     append 1 "<li>[_ intranet-translation.lt_You_have_insufficient_3]"
     return
 }
+
+# ---------------------------------------------------------------------
+# Get the file and deal with Unicode encoding...
+# ---------------------------------------------------------------------
+
+ns_log Notice "trados-import: trados_wordcount_file=$trados_wordcount_file"
+
+if {[catch {
+    set fl [open $trados_wordcount_file]
+    fconfigure $fl -encoding binary
+    set binary_content [read $fl]
+    close $fl
+} err]} {
+    ad_return_complaint 1 "Unable to open file $trados_wordcount_file:<br><pre>\n$err</pre>"
+    return
+}
+
+set encoding_bin [string range $binary_content 0 1]
+binary scan $encoding_bin H* encoding_hex
+ns_log Notice "trados-import: encoding_hex=$encoding_hex"
+
+switch $encoding_hex {
+    fffe {
+	# Assume a UTF-16 file
+	set encoding "unicode"
+    }
+    default {
+	# Assume a UTF-8 file
+	set encoding "utf-8"
+    }
+}
+
 
 set target_language_ids [im_target_language_ids $project_id]
 if {0 == [llength $target_language_ids]} {
@@ -97,7 +127,18 @@ append page_body "
 <table cellpadding=0 cellspacing=2 border=0>
 "
 
-set trados_files_content [exec /bin/cat $trados_wordcount_file]
+
+if {[catch {
+    set fl [open $trados_wordcount_file]
+    fconfigure $fl -encoding $encoding 
+    set trados_files_content [read $fl]
+    close $fl
+} err]} {
+    ad_return_complaint 1 "Unable to open file $trados_wordcount_file:<br><pre>\n$err</pre>"
+    return
+}
+
+# set trados_files_content [exec /bin/cat $trados_wordcount_file]
 set trados_files [split $trados_files_content "\n"]
 set trados_files_len [llength $trados_files]
 set trados_header [lindex $trados_files 1]
@@ -118,8 +159,6 @@ if {1 == [llength $trados_headers]} {
 set line2 [lindex $trados_files 2]
 set line2_len [llength [split $line2 $separator]]
 
-
-
 if {";" == $separator} {
     switch $line2_len {
 	40 { set trados_version "6.5" }
@@ -130,11 +169,14 @@ if {";" == $separator} {
     }
 } else {
     switch $line2_len {
+	41 { set trados_version "7.0" }
+	40 { set trados_version "6.5" }
 	39 { set trados_version "6.0" }
 	default { set trados_version "unknown" }
     }
 }
 
+ns_log Notice "trados-import: trados_version=$trados_version, line2_len=$line2_len, separator=$separator"
 
 
 append page_body "
@@ -178,17 +220,17 @@ if {[string equal "unknown" $trados_version]} {
 # This procedure is not necessary for LocalFs import.
 set common_filename_comps 0
 
-ns_log Notice "import_method=$import_method"
+ns_log Notice "trados-import: import_method=$import_method"
 if {[string equal $import_method "Asp"]} {
     set first_trados_line [lindex $trados_files 2]
     set first_trados_fields [split $first_trados_line $separator]
     set first_filename [lindex $first_trados_fields 0]
     set first_filename_comps [split $first_filename "\\"]
 
-    ns_log Notice "first_trados_line=$first_trados_line"
-    ns_log Notice "first_trados_fields=$first_trados_fields"
-    ns_log Notice "first_filename=$first_filename"
-    ns_log Notice "first_filename_comps=$first_filename_comps"
+    ns_log Notice "trados-import: first_trados_line=$first_trados_line"
+    ns_log Notice "trados-import: first_trados_fields=$first_trados_fields"
+    ns_log Notice "trados-import: first_filename=$first_filename"
+    ns_log Notice "trados-import: first_filename_comps=$first_filename_comps"
 
     set all_the_same 1
     set ctr 0
@@ -204,7 +246,7 @@ if {[string equal $import_method "Asp"]} {
 	    set filename [lindex $trados_fields 0]
 	    set filename_comps [split $filename "\\"]
 	    set this_component [lindex $filename_comps $ctr]
-	    ns_log Notice "this_component=$this_component"
+	    ns_log Notice "trados-import: this_component=$this_component"
 
 	    if {![string equal $common_component $this_component]} {
 		set all_the_same 0
@@ -217,7 +259,7 @@ if {[string equal $import_method "Asp"]} {
     set common_filename_comps [expr $ctr - 1]
 }
 
-ns_log Notice "common_filename_comps=$common_filename_comps"
+ns_log Notice "trados-import: common_filename_comps=$common_filename_comps"
 
 
 
@@ -258,7 +300,7 @@ ns_log Notice "common_filename_comps=$common_filename_comps"
 	    set p0_placeables	[lindex $trados_fields 23]
 	}
 
-	if {[string equal $trados_version "5.5"] || [string equal $trados_version "5.0"] || [string equal $trados_version "6.0"] | [string equal $trados_version "6.5"]} {
+	if {[string equal $trados_version "5.5"] || [string equal $trados_version "5.0"] || [string equal $trados_version "6.0"] || [string equal $trados_version "6.5"] || [string equal $trados_version "7.0"]} {
 
 	    set px_segments	[lindex $trados_fields 3]
 	    set px_words	[lindex $trados_fields 4]
