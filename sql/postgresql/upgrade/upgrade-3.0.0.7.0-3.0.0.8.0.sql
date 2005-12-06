9647c60887c5bfebc3d92e7a1076eeb1f9b17b31
Klaus Hofeditz <klaus.hofeditz@project-open.com>
2005-12-06 22:00:52 +0000

    - added column to project list page to show generic size field

--- sql/postgresql/upgrade/upgrade-3.0.0.7.0-3.0.0.8.0.sql
+++ sql/postgresql/upgrade/upgrade-3.0.0.7.0-3.0.0.8.0.sql
@@ -249,3 +249,14 @@ extra_select, extra_where, sort_order, visible_for) values (9014,90,NULL,'End Da
 '','',14,'expr !$project_write');
 
 
+
+-- Replace the previous Project Size field with a more generic
+-- one that takes into account T-Lines etc.
+--
+delete from im_view_columns where column_id = 2023;
+--
+insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
+extra_select, extra_where, sort_order, visible_for) values (2023,20,NULL,'Size',
+'$trans_size','','',90,'');
+
+
