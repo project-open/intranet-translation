57e460154d0d3dd0f8bf1403af7150ddddde2c7d
lup <>
2006-01-04 20:46:31 +0000

    - "Size" column of the project list is now only visible for "view_trans_proj_detail"

--- sql/common/intranet-translation-common.sql
+++ sql/common/intranet-translation-common.sql
@@ -29,8 +29,7 @@
 --
 insert into im_view_columns (column_id, view_id, group_id, column_name, column_render_tcl,
 extra_select, extra_where, sort_order, visible_for) values (2023,20,NULL,'Size',
-'$trans_size'','','',90,'');
-
+'$trans_size','','',90,'im_permission $user_id view_trans_proj_detail');
 
 
 
